c542c1be8ad1d31ad1d11f83083c570b704e3281
ravindersingh1979 <105660294+ravindersingh1979@users.noreply.github.com>
2023-02-08 12:12:04 +0530

    fetch part2

---
+++ fetch-promise-02-example/public/src/js/app.js
@@ -0,0 +1,49 @@
+var deferredPrompt;
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/sw.js").then(function () {
+    console.log("Service worker registered!");
+  });
+}
+
+window.addEventListener("beforeinstallprompt", function (event) {
+  console.log("beforeinstallprompt fired");
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
+
+setTimeout(function () {
+  console.log("This is executed once the timer is done!");
+}, 3000);
+
+console.log("This is executed right after setTimeout()");
+
+var promise = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    //resolve('This is executed once the timer is done!');
+    reject({ code: 500, message: "An error occurred!" });
+    //console.log('This is executed once the timer is done!');
+  }, 3000);
+});
+
+// promise.then(function(text) {
+//   return text;
+// }, function(err) {
+//   console.log(err.code, err.message)
+// }).then(function(newText) {
+//   console.log(newText);
+// });
+
+promise
+  .then(function (text) {
+    return text;
+  })
+  .then(function (newText) {
+    console.log(newText);
+  })
+  .catch(function (err) {
+    console.log(err.code, err.message);
+  });
+
+console.log("This is executed right after setTimeout()");
